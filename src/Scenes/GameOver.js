6132902ded9f30f3d664d8357f0269df322d7bd2
Felix23333 <67811692+Felix23333@users.noreply.github.com>
2021-04-29 16:07:07 +0800

    Update GameOver.js

--- src/Scenes/GameOver.js
+++ src/Scenes/GameOver.js
@@ -14,10 +14,16 @@ class GameOver extends Phaser.Scene
     {
         console.log("End Game!");
         this.input.keyboard.on('keydown-M', this.BackToMain, this);
+        this.input.keyboard.on('keydown-R', this.Restart, this);
     }
 
     BackToMain()
     {
         this.scene.start("menuScene");
+    }
+
+    Restart()
+    {
+        this.scene.start("playScene");
     }
 }
