81c1643a8c177f3b3de56ad9dc288cda055f8f75
Felix Li <lbc1125wj@gmail.com>
2021-05-03 02:07:01 -0700

    add music copyright comment

--- src/Scenes/MainMenu.js
+++ src/Scenes/MainMenu.js
@@ -7,9 +7,15 @@ class MainMenu extends Phaser.Scene
 
     preload()
     {
+        //The hurt effect is reused from my CMPM80K game project
         this.load.audio("hurt", "assets/player-hurt.wav");
+        //The menu & play music is from royalty free music,
+        //epidemicsound.com/music/genres/electro/?_us=adwords&_usx=11282646959_&gclid=CjwKCAiA9vOABhBfEiwATCi7GHNKfLA29qr2qCiNelpEUg-gbymFchVhzs1kjlT53VkqWRp6M_jZuxoCiK8QAvD_BwE
         this.load.audio("backgroundMusic", "assets/backgroundmusic.mp3");
         this.load.audio("menuMusic", "assets/intromusic.mp3");
+        //The collect sfx is modified by
+        //https://www.videvo.net/sound-effect/water-lapping-closeu-b-t053901/261257/
+        //https://www.videvo.net/sound-effect/human-swallow-02/429168/
         this.load.audio("sfx_collection", "assets/collectorEffect.mp3");
     }
 
